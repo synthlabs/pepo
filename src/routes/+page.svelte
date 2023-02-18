88d17aef6c9f3750f4bc6cad876e08d6e0d58abe
Jerod <jerod@synth.sh>
2023-02-18 15:16:35 -0800

    add twitch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,38 @@
 <script lang="ts">
-	import Eliza from 'elizabot';
-	import { beforeUpdate, afterUpdate } from 'svelte';
+	import { beforeUpdate, afterUpdate, onDestroy } from 'svelte';
+	import { StaticAuthProvider } from '@twurple/auth';
+	import { ChatClient } from "@twurple/chat";
+	import {ClientID, AccessToken} from "$lib/config/config";
 
 	let div: HTMLDivElement;
 	let autoscroll: boolean;
 
-	let channel = "#hasanabi";
+	let channel = "hasanabi";
+	let behavior: ScrollBehavior = "auto";
+
+	const authProvider = new StaticAuthProvider(ClientID, AccessToken);
+	const chatClient = new ChatClient({ authProvider, channels: [channel] });
+
+	chatClient.connect().then(()=> {console.log("connected")})
+	onDestroy(() => {
+		chatClient.quit()
+	});
+
+	interface message {
+		ts: string;
+		username: string;
+		message: string;
+	}
+	let messages: message[] = [];
+
+	chatClient.onMessage((channel, user, text, msg) => {
+		let m = {
+			ts: msg.date.toLocaleTimeString(),
+			username: msg.userInfo.displayName,
+			message: text,
+		}
+		messages = [...messages, m]
+	})
 
 	beforeUpdate(() => {
 		// determine whether we should auto-scroll
@@ -15,47 +42,15 @@
 
 	afterUpdate(() => {
 		// ...the DOM is now in sync with the data
-		if (autoscroll) div.scrollTo(0, div.scrollHeight);
-	});
-
-	const eliza = new Eliza();
-
-	let comments = [
-		{ author: 'eliza', text: eliza.getInitial(), placeholder: false}
-	];
-
-	function handleKeydown(event: { key: string; target: { value: string; }; }) {
-		if (event.key === 'Enter') {
-			const text = event.target.value;
-			if (!text) return;
-
-			comments = comments.concat({
-				author: 'user',
-				text,
-				placeholder: false,
+		if (autoscroll) {
+			div.scrollTo({
+				top: div.scrollHeight,
+				left: 0,
+				behavior,
 			});
-
-			event.target.value = '';
-
-			const reply = eliza.transform(text);
-
-			setTimeout(() => {
-				comments = comments.concat({
-					author: 'eliza',
-					text: '...',
-					placeholder: true
-				});
-
-				setTimeout(() => {
-					comments = comments.filter(comment => !comment.placeholder).concat({
-						author: 'eliza',
-						text: reply,
-						placeholder: false,
-					});
-				}, 500 + Math.random() * 500);
-			}, 200 + Math.random() * 200);
 		}
-	}
+	});	
+
 </script>
 
 <style>
@@ -63,13 +58,17 @@
 </style>
 
 <div class="flex flex-col h-full">
-	<h1>{channel}</h1>
+	<h1>#{channel}</h1>
 
 	<div class="flex-1 overflow-y-auto" bind:this={div}>
-		<article>
-			<span>How Do you do you bitch</span>
-		</article>
+		{#each messages as msg}
+			<div>
+				<span class="text-primary text-opacity-80">{msg.ts}</span>
+				<span class="text-secondary">{msg.username}</span><span>:</span>
+				<span>{msg.message}</span>
+			</div>
+		{/each}
 	</div>
 
-	<input on:keydown={handleKeydown} placeholder="write your love letter here">
+	<input placeholder="write your love letter here">
 </div>
