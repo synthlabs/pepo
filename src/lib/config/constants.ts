72f61bcdf63d492fdc7ef22fe2d06e687ff1822e
Jerod <jerod@synth.sh>
2023-04-12 10:59:32 -0700

    add simple dockerfile

---
+++ src/lib/config/constants.ts
@@ -0,0 +1,3 @@
+export const TEXT_TOKEN = "text";
+export const EMOTE_TOKEN = "emote";
+export const CHEER_TOKEN = "cheer";
---
+++ src/routes/[channel]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Logger from '$lib/logger/log';
+
+	Logger.debug($page);
+</script>
+
+<pre>
+    {JSON.stringify($page, null, 2)}
+</pre>
