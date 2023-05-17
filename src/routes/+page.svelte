fc7fabcf0ad3bb8d8a28976c3ba0dd1369f4b119
Jerod <jerod@synth.sh>
2023-05-17 08:16:33 -0700

    feat(landing): input to send you to a channel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<script>
-	import Chat from '$lib/components/chat/+chat.svelte';
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Logger from '$lib/logger/log';
+
+	let inputStr = '';
+	function submitForm() {
+		Logger.debug(inputStr);
+		goto(`/${inputStr}`);
+	}
 </script>
+
+<div class="flex flex-col items-center justify-center w-full h-screen">
+	<form class="w-full flex" on:submit|preventDefault={submitForm}>
+		<input
+			type="text"
+			bind:value={inputStr}
+			class="input input-bordered m-auto w-1/2"
+			placeholder="Where would you like to go?"
+		/>
+	</form>
+</div>
