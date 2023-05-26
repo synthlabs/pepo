2fcd9a100378ff84cf5757570bee2c992ba018e1
Jerod <jerod@synth.sh>
2023-05-26 07:31:10 -0700

    feat(components): move the channel switcher input to its own component

---
+++ src/lib/components/+inputnav.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let inputStr = '';
+	function submitForm() {
+		goto(`/chat/${inputStr}`);
+	}
+</script>
+
+<div class="flex flex-col flex-grow w-full items-center justify-center">
+	<form class="w-full flex" on:submit|preventDefault={submitForm}>
+		<input
+			type="text"
+			bind:value={inputStr}
+			class="input input-bordered m-auto w-1/2"
+			placeholder="Where would you like to go?"
+		/>
+	</form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import Logger from '$lib/logger/log';
-
-	let inputStr = '';
-	function submitForm() {
-		goto(`/chat/${inputStr}`);
-	}
+	import InputNav from '$lib/components/+inputnav.svelte';
 </script>
 
 <div class="flex flex-col items-center justify-center w-full h-screen">
-	<form class="w-full flex" on:submit|preventDefault={submitForm}>
-		<input
-			type="text"
-			bind:value={inputStr}
-			class="input input-bordered m-auto w-1/2"
-			placeholder="Where would you like to go?"
-		/>
-	</form>
+	<InputNav />
 </div>
