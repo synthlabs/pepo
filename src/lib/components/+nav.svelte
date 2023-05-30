d3e426bb7f5c7c29e63f99a76f95e2437c22b478
Jerod <jerod@synth.sh>
2023-05-30 09:26:59 -0700

    feat(desktop): initial desktop functionality

--- src/lib/components/+nav.svelte
+++ src/lib/components/+nav.svelte
@@ -14,24 +14,41 @@
 
 	{#if $user}
 		<div class="flex-shrink-0 w-1/2"><InputNav /></div>
-
-		<div class="gap-2 pr-1 w-1/2 justify-end items-end">
-			<div class="dropdown dropdown-end">
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-					<div class="w-9 rounded-full">
-						<img src={$user?.profilePictureUrl} alt={$user?.displayName} />
-					</div>
-				</label>
-				<ul
-					tabindex="0"
-					class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52"
-				>
-					<li><a href="/settings">Settings</a></li>
-				</ul>
-			</div>
-		</div>
 	{/if}
+
+	<div class="gap-2 pr-1 w-1/2 justify-end items-end">
+		<div class="dropdown dropdown-end">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+				<div class="w-9 rounded-full">
+					{#if $user}
+						<img src={$user?.profilePictureUrl} alt={$user?.displayName} />
+					{:else}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-9 h-9"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+							/>
+						</svg>
+					{/if}
+				</div>
+			</label>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52"
+			>
+				<li><a href="/settings">Settings</a></li>
+			</ul>
+		</div>
+	</div>
 </div>
 
 <style>
