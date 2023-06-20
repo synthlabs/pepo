9f4fc9077a1792bdf967f0dd315a3c7dc8c2b8c4
Jerod <7307833+xjerod@users.noreply.github.com>
2023-06-20 07:32:57 -0700

    chore(deps): update deps 06-20-2023 (#12)

---
+++ src/lib/components/+tabs.svelte
@@ -0,0 +1,119 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Logger from '$lib/logger/log';
+	import { channels as channelCache, Sanitize } from '$lib/store/channels';
+
+	channelCache.useLocalStorage();
+
+	let inputStr = '';
+	let modalOpen = false;
+	let channels = $channelCache ? $channelCache : [];
+	let channelInput: HTMLInputElement;
+	let modal: HTMLDialogElement;
+
+	// TODO: move this into a more central keybindings module
+	// 		 https://www.reddit.com/r/sveltejs/comments/tp7hul/comment/i29svqt/
+
+	function handleEscape(event: KeyboardEvent) {
+		if (event.key === 'Escape') {
+			inputStr = '';
+			modalOpen = false;
+		}
+	}
+
+	function handleSave() {
+		if (inputStr.length > 0) {
+			const chan = Sanitize(inputStr);
+			Logger.debug(chan);
+			channels = [...channels, chan];
+			channelCache.set(channels);
+			closeDialog();
+		}
+	}
+
+	function openDialog() {
+		modal.showModal();
+		channelInput.focus();
+	}
+
+	function closeDialog() {
+		inputStr = '';
+		modal.close();
+	}
+</script>
+
+<svelte:window on:keyup={handleEscape} />
+
+<div class="tabs flex-shrink-0 bg-base-100 w-full gap-1 pl-1 pt-1 pr-1">
+	{#each channels as name}
+		<a class="tab" class:tab-active={name === $page.params.channel} href="/chat/{name}">{name}</a>
+	{/each}
+	<button
+		on:click={openDialog}
+		class="inline-flex flex-wrap items-center justify-center text-center plus cursor-pointer"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			class="w-6 h-6"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+	</button>
+</div>
+
+<dialog id="add-channel" class="modal" bind:this={modal}>
+	<form method="dialog" class="modal-box" on:submit|preventDefault={handleSave}>
+		<label
+			for="add-channel"
+			on:click={closeDialog}
+			class="btn btn-xs btn-circle absolute right-2 top-2">✕</label
+		>
+		<h3 class="font-bold text-md">Add a new favorite channel</h3>
+		<!-- TODO: make this submit by hitting enter -->
+		<!-- TODO: This should auto focus too -->
+		<input
+			type="text"
+			bind:this={channelInput}
+			bind:value={inputStr}
+			class="input input-bordered m-auto w-full input-sm mt-6"
+			placeholder="Channel name"
+		/>
+		<div class="modal-action">
+			<label for="add-channel" class="btn btn-sm" on:click={handleSave}>Save</label>
+		</div>
+	</form>
+</dialog>
+
+<style>
+	.tab {
+		border-radius: 0.5rem /* 8px */;
+		--tw-bg-opacity: 1;
+		background-color: hsl(var(--b2, var(--b1)) / var(--tw-bg-opacity));
+	}
+	.tab-active {
+		--tw-bg-opacity: 1;
+		background-color: hsl(var(--p) / var(--tw-bg-opacity));
+	}
+	.plus {
+		height: 2rem /* 32px */;
+		font-size: 0.875rem /* 14px */;
+		line-height: 1.25rem /* 20px */;
+		line-height: 2;
+		--tab-padding: 0.5rem /* 16px */;
+		--tw-text-opacity: 0.5;
+		--tab-color: hsla(var(--bc) / var(--tw-text-opacity, 1));
+		--tab-bg: hsla(var(--b1) / var(--tw-bg-opacity, 1));
+		--tab-border-color: hsla(var(--b3) / var(--tw-bg-opacity, 1));
+		color: var(--tab-color);
+		padding-left: var(--tab-padding, 1rem);
+		padding-right: var(--tab-padding, 1rem);
+	}
+</style>
