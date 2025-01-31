6207aa24fd01f078ac315e2cfb1992cb5c809f25
Jerod <jerod@synth.sh>
2025-01-31 15:10:24 -0800

    working sidebar

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-import tailwindcss from '@tailwindcss/vite'
 
 
 // @ts-expect-error process is a nodejs global
@@ -8,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    plugins: [tailwindcss(), sveltekit()],
+    plugins: [sveltekit()],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
