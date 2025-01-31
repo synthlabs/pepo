ae354e22528bb5cf449175ad6516da25a979a1f1
Jerod <jerod@synth.sh>
2025-01-30 19:59:34 -0800

    shadcn

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,13 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  },
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $lib: 'src/lib',
+        }
+    },
 };
 
 export default config;
