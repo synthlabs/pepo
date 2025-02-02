69471e982af26d634faa5521656f4f2e3796e4ce
Jerod <jerod@synth.sh>
2025-02-01 19:39:55 -0800

    fix layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,20 @@
 	import '../app.css';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.ts';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import { Separator } from '$lib/components/ui/separator/index.ts';
 
 	let { children } = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar></AppSidebar>
-	<main>
-		<Sidebar.Trigger></Sidebar.Trigger>
-		{@render children?.()}
+	<main class="w-full">
+		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+			<Sidebar.Trigger class="-ml-1" />
+			<Separator orientation="vertical" class="mr-2 h-4" />
+		</header>
+		<div class="flex flex-1 flex-col">
+			{@render children?.()}
+		</div>
 	</main>
 </Sidebar.Provider>
