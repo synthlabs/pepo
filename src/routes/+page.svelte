9462f4c802a9f595317c6bdf6a9f6e232b109ab3
Jerod <jerod@synth.sh>
2025-02-02 16:33:32 -0800

    linting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Separator } from '$lib/components/ui/separator/index.ts';
 
-	const tags = Array.from({ length: 55 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
+	const msgs = Array.from({ length: 55 }).map(
+		(_, i, a) => `12:3${i % 10}pm twitch_user${i % 6}: bingo bang, bazinga`
+	);
 
 	const evenOddClass = (x: number): string => {
 		if (x % 2 === 0) {
@@ -13,9 +15,9 @@
 
 <div class="flex h-full w-full flex-col flex-nowrap">
 	<div class="flex-grow overflow-y-auto overflow-x-hidden">
-		{#each tags as tag, index}
+		{#each msgs as msg, index}
 			<div class="p-2 text-sm" style={evenOddClass(index)}>
-				{tag}
+				{msg}
 			</div>
 			<Separator class="" />
 		{/each}
