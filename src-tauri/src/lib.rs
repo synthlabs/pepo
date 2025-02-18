92ec4b571d746967d496bc567f519298db53a887
Jerod <jerod@synth.sh>
2025-02-17 16:36:39 -0800

    basic twitch auth, mock out more ui elements

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,80 @@
+use std::sync::Mutex;
+
 use serde::{Deserialize, Serialize};
 use specta::Type;
 use specta_typescript::Typescript;
-use tauri::Emitter;
 #[cfg(target_os = "macos")]
 use tauri::TitleBarStyle;
+use tauri::{Emitter, Manager};
 use tauri::{WebviewUrl, WebviewWindowBuilder};
+use tauri_plugin_opener::OpenerExt;
 use tauri_specta::collect_commands;
+use twitch_api::{client::ClientDefault, HelixClient};
+use twitch_oauth2::Scope;
+
+mod types;
+
+const CLIENT_ID: &str = "46oisb828x3q9lu42ctuhphonrlho9";
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 #[specta::specta]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+#[specta::specta]
+async fn login(app_handle: tauri::AppHandle) -> Result<types::UserToken, String> {
+    println!("login");
+
+    let client: HelixClient<reqwest::Client> = twitch_api::HelixClient::with_client(
+        ClientDefault::default_client_with_name(Some("pepo".parse().expect("invalid client name")))
+            .unwrap(),
+    );
+
+    println!("made client");
+
+    // First we need to get a token, preferably you'd also store this information somewhere safe to reuse when restarting the application.
+    // For now we'll just get a new token every time the application starts.
+    // One way to store the token is to store the access_token and refresh_token in a file and load it when the application starts with
+    // `twitch_oauth2::UserToken::from_existing`
+    let mut builder = twitch_oauth2::tokens::DeviceUserTokenBuilder::new(
+        CLIENT_ID.to_string(),
+        vec![
+            Scope::UserReadChat,
+            Scope::UserWriteChat,
+            Scope::UserReadFollows,
+            Scope::UserReadEmotes,
+            Scope::UserReadBlockedUsers,
+            Scope::UserReadSubscriptions,
+        ],
+    );
+
+    println!("made token builder");
+
+    let code = match builder.start(&client).await {
+        Ok(code) => code,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    println!("Please go to: {}", code.verification_uri);
+    app_handle
+        .opener()
+        .open_url(code.verification_uri.clone(), None::<&str>)
+        .unwrap();
+
+    let token = builder
+        .wait_for_code(&client, tokio::time::sleep)
+        .await
+        .map_err(|err| err.to_string())?;
+
+    let user_token = types::UserToken::from_twitch_token(token.clone());
+    println!("{:#?}", user_token);
+
+    app_handle.manage(Mutex::new(user_token.clone()));
+
+    Ok(user_token)
 }
 
 #[derive(Serialize, Deserialize, Type)]
@@ -25,12 +88,19 @@ pub fn run() {
 
     let handlers = tauri_specta::Builder::<tauri::Wry>::new()
         .typ::<MyStruct>()
+        .typ::<types::UserToken>()
         // Then register them (separated by a comma)
-        .commands(collect_commands![greet,]);
+        .commands(collect_commands![greet, login,]);
 
     #[cfg(debug_assertions)] // <- Only export on non-release builds
     handlers
-        .export(Typescript::default(), "../src/lib/bindings.ts")
+        .export(
+            Typescript::default()
+                .formatter(specta_typescript::formatter::prettier)
+                .bigint(specta_typescript::BigIntExportBehavior::BigInt)
+                .header("/* eslint-disable */"),
+            "../src/lib/bindings.ts",
+        )
         .expect("Failed to export typescript bindings");
 
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
@@ -51,7 +121,7 @@ pub fn run() {
             let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default());
 
             #[cfg(not(any(target_os = "android", target_os = "ios")))]
-            let win_builder = win_builder.title("").inner_size(1200.0, 600.0);
+            let win_builder = win_builder.title("").inner_size(1000.0, 600.0);
 
             // set transparent title bar only when building for macOS
             #[cfg(target_os = "macos")]
