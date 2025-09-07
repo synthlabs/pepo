7881fce2fef12d501b9fd2a5918074e5d97ab983
Jerod <jerod@synth.sh>
2025-09-07 10:31:12 -0700

    move sumry out of repo

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-ROOT=$(git rev-parse --show-toplevel)
-SUMRY_BIN="${ROOT}/bin/sumry"
-SUMRY_SRC="${ROOT}/scripts/sumry"
-
-echo "Project root ${ROOT}"
-echo "Sumry bin ${SUMRY_BIN}"
-echo "Sumry src ${SUMRY_SRC}"
-
-set -ex
-
-pushd $SUMRY_SRC
-go build -o $SUMRY_BIN ./main.go
-popd
-
 EXTRA_ARGS="-project-root ${ROOT} -debug"
 
-${SUMRY_BIN} $(echo "${EXTRA_ARGS} $@" | awk '{$1=$1};1')
+sumry $(echo "${EXTRA_ARGS} $@" | awk '{$1=$1};1')
