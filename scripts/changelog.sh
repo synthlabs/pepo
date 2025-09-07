bc17735c761882989ccb4ad76b33b06e357249b5
Jerod <jerod@synth.sh>
2025-09-07 11:50:16 -0700

    move vergo out of repo

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 EXTRA_ARGS="-project-root ${ROOT} -debug"
 
 sumry $(echo "${EXTRA_ARGS} $@" | awk '{$1=$1};1')
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-ROOT=$(git rev-parse --show-toplevel)
-VERGO_BIN="${ROOT}/bin/vergo"
-VERGO_SRC="${ROOT}/scripts/vergo"
-
-echo "Project root ${ROOT}"
-echo "Vergo bin ${VERGO_BIN}"
-echo "Vergo src ${VERGO_SRC}"
-
 set -ex
-
-pushd $VERGO_SRC
-go build -o $VERGO_BIN ./main.go
-popd
 
 EXTRA_ARGS="-project-root ${ROOT} -debug"
 
-${VERGO_BIN} $(echo "${EXTRA_ARGS} $@" | awk '{$1=$1};1')
+vergo $(echo "${EXTRA_ARGS} $@" | awk '{$1=$1};1')
