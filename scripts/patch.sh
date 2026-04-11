b8cfc2aa393a2825e6dfaa32257678f2c25acddd
Jerod <jerod@synth.sh>
2026-04-11 12:56:05 -0700

    fix(build): extend patch script

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 ROOT=$(git rev-parse --show-toplevel)
 
-vergo -project-root ${ROOT} -debug -update
-sumry -project-root ${ROOT} -debug -update
+vergo -project-root "${ROOT}" -debug -update
+VERSION=$(jq -r '.version' "${ROOT}/package.json")
+sumry -project-root "${ROOT}" -debug -update
+
+git add \
+  "${ROOT}/package.json" \
+  "${ROOT}/src-tauri/Cargo.toml" \
+  "${ROOT}/src-tauri/Cargo.lock" \
+  "${ROOT}/src-tauri/tauri.conf.json" \
+  "${ROOT}/SUMRY.md" \
+  "${ROOT}/archive/"
+
+git commit -m "chore(updater): version bump ${VERSION}"
