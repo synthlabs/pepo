304e2130e374c41fce30c3bf13af8a834a43c12a
Jerod <jerod@synth.sh>
2026-06-17 17:23:26 -0700

    fix(ui): tooltips now properly render above content

--- src/lib/components/ui/tooltip/tooltip-content.svelte
+++ src/lib/components/ui/tooltip/tooltip-content.svelte
@@ -6,16 +6,21 @@
 		ref = $bindable(null),
 		class: className,
 		sideOffset = 4,
+		portalProps,
 		...restProps
-	}: TooltipPrimitive.ContentProps = $props();
+	}: TooltipPrimitive.ContentProps & {
+		portalProps?: TooltipPrimitive.PortalProps;
+	} = $props();
 </script>
 
-<TooltipPrimitive.Content
-	bind:ref
-	{sideOffset}
-	class={cn(
-		'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-		className
-	)}
-	{...restProps}
-/>
+<TooltipPrimitive.Portal {...portalProps}>
+	<TooltipPrimitive.Content
+		bind:ref
+		{sideOffset}
+		class={cn(
+			'z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+			className
+		)}
+		{...restProps}
+	/>
+</TooltipPrimitive.Portal>
